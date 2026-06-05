23dcdb2b8b6288832a6c832c10ff909076af341d
Kyle Cooke <kyle@playerdata.com>
2026-06-05 14:11:40 +0100

    feat: add env variables that can override urls for development

--- playerdatapy/constants.py
+++ playerdatapy/constants.py
@@ -1,1 +1,4 @@
-API_BASE_URL = "https://app.playerdata.co.uk"
+import os
+
+API_BASE_URL = os.environ.get("PLAYERDATA_BASE_URL", "https://app.playerdata.co.uk")
+GRAPHQL_URL = os.environ.get("PLAYERDATA_GRAPHQL_URL", f"{API_BASE_URL}/api/graphql")
